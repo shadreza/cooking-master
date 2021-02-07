be34f907b72941a01537920251cf32354d2d7b11
shadreza <shadreza100@gmail.com>
2021-02-07 23:38:38 +0600

    Alhamdulillah inspite of the math245 final ALLAH made me finish it

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+// to show a message using a custom popup box 
 const showMessage = message => {
     document.getElementById('popupBoxMessage').innerText=message;
     document.getElementById('popupBox').style.display='block';
 }
 
+// closing the popup box from the buttons click
 document.getElementById('closePopupBox').addEventListener('click',function(){
     document.getElementById('popupBox').style.display='none';
 })
 
+// this will display the foods in a grid of 4 and this will be done at the first stage of the webpage loading
+// also the image and the name will be shown 
 const displayFoodsInTheGallery = fletchedJsonDataOfFoods => {
     foodArrayFromJsonMeals = fletchedJsonDataOfFoods.meals;
     foodArrayFromJsonMeals.forEach(individualFoodContent => {
@@ -24,7 +28,11 @@ const displayFoodsInTheGallery = fletchedJsonDataOfFoods => {
     });
 }
 
+// this functions holds the power of dsiplaying a hidden div with the name , picture , ingredients and the measurements and also 2 special buttons like a button for video and closing the div 
+// here all the elements re being created and after reading the ingredients and the measures all the elements are appended to the main div
 const showTheDetailsOfTheFood = fletchedDataOfFood => {
+
+    // setting up the tags
     const selectedFoodShowCasingDiv = document.getElementById('foodThatWillShowUpId');
     const foodImageUrl = fletchedDataOfFood.strMealThumb;
     const nameOfTheFood = fletchedDataOfFood.strMeal;
@@ -47,6 +55,8 @@ const showTheDetailsOfTheFood = fletchedDataOfFood => {
         selectedFoodShowCasingDiv.innerHTML='';
         selectedFoodShowCasingDiv.style.display='none';
     })
+
+    // as the ingredients are in strIngredient1 , strIngredient2 , strIngredient(n) format so i had to use the for loop to bring the properties out like that
     let i=1;
     while(1){
         const ingredientsProperty = `strIngredient${i}`;
@@ -64,10 +74,13 @@ const showTheDetailsOfTheFood = fletchedDataOfFood => {
         i++;
     }
     if(i==1){
+        // if the loop never ran this will happen only if there is no ingredients so no need to go further
         showMessage('No Ingredients Found!');
         return;
     }
     else{
+
+        // appending the html tags in the detail div portion 
         const beforeIngredientsText = document.createElement('h5');
         beforeIngredientsText.innerText = 'The Ingredients Along with their Measurements ->>>\n';
         selectedFoodShowCasingDiv.innerHTML=''; 
@@ -83,10 +96,12 @@ const showTheDetailsOfTheFood = fletchedDataOfFood => {
     }
 }
 
+// this is a function that call for the special div or the details section of a given items or food
 const requestForInternalDetails = urlOfTheSpecificFood => {
     fetchingFromTheMealDBApi(urlOfTheSpecificFood , 2);
 }
 
+// if any part of the foodGallery is pressed then it will trigger the event and if the picture or the name or the internal part of the food is clicked this will show the details of the food but if the outside part or a non food part is clicked then this will not go for the details section
 document.getElementById('foodGallery').addEventListener('click',event =>{
     const idOfTheClickedFood = event.target.id;
     if(idOfTheClickedFood == 'foodGallery'){
@@ -96,6 +111,9 @@ document.getElementById('foodGallery').addEventListener('click',event =>{
     requestForInternalDetails(sendingAPIUrl);
 })
 
+// the fletching part is done in this functions
+// if the second arguement or passedInfoForTakingDecision is 1 that means loading the foods for the initial display
+// else if passedInfoForTakingDecision is 2 that means i am searching for a specific food 
 const fetchingFromTheMealDBApi = (passedUrlForFetching , passedInfoForTakingDecision) => {
     fetch(passedUrlForFetching)
     .then(responseFromApiToJson => responseFromApiToJson.json())
@@ -121,12 +139,16 @@ const fetchingFromTheMealDBApi = (passedUrlForFetching , passedInfoForTakingDeci
     })
 }
 
+// this is like the constructor portion of the code 
+// this will display the foods in the initial stages
+// displaying foods that starts with a, c, d, e and f
 fetchingFromTheMealDBApi('https://www.themealdb.com/api/json/v1/1/search.php?f=a',1);
 fetchingFromTheMealDBApi('https://www.themealdb.com/api/json/v1/1/search.php?f=c',1);
 fetchingFromTheMealDBApi('https://www.themealdb.com/api/json/v1/1/search.php?f=d',1);
 fetchingFromTheMealDBApi('https://www.themealdb.com/api/json/v1/1/search.php?f=e',1);
 fetchingFromTheMealDBApi('https://www.themealdb.com/api/json/v1/1/search.php?f=f',1);
 
+// this function looks inside the search box input section and if it sees a valid string then it sends that name or text for showing the details about that food or object and if not found then shown message
 const searchMealInfo = () => {
     const nameOfTheEnteredMeal = document.getElementById('inputFromUser').value;
     if(nameOfTheEnteredMeal=='' || nameOfTheEnteredMeal==undefined || nameOfTheEnteredMeal==null){
@@ -138,15 +160,18 @@ const searchMealInfo = () => {
     }
 }
 
+// this will clear up the input text or the search box
 const clearTheSearchBox = () => {
     document.getElementById('inputFromUser').value='';
 }
 
+// when the search button is pressed this triggers the searching of the entered information
 document.getElementById('searchButton').addEventListener('click',function(){
     searchMealInfo();
     clearTheSearchBox();
 })
 
+// if enter is pressed and the focus is at the time of staying in the input frame this will also trigger the searching for the meal information
 document.getElementById('inputFromUser').addEventListener('keydown',function(event){
     if(event.keyCode===13){
         searchMealInfo();
